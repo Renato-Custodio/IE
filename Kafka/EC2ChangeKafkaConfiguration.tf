ac94645656d5544429d0d560327dcb930018482f
Renato Miguel Monteirinho Custódio <renato.miguel.monteirinho.custodio@tecnico.ulisboa.pt>
2025-05-13 12:48:39 +0100

    implementação do cluster com 3 brokers baseado no que esta no lab

--- Kafka/EC2ChangeKafkaConfiguration.tf
+++ Kafka/EC2ChangeKafkaConfiguration.tf
@@ -10,25 +10,35 @@ terraform {
 }
 
 provider "aws" {
-  region      = "us-east-1"
-  }
-
-resource "aws_instance" "exampleKafkaConfiguration" {
-  ami                     = "ami-045269a1f5c90a6a0"
-  instance_type           = "t2.small"
-  count                   = 1
-  vpc_security_group_ids  = [aws_security_group.instance.id]
-  key_name                = "vockey"
-
-  user_data = "${file("creation.sh")}"
-
-  user_data_replace_on_change = true
-  
-  tags = {
-    Name = "terraform-kafka"
-  }
+  region = "us-east-1"
 }
 
+variable "nBroker" {
+  description = "number of brokers"
+  type        = number
+  default     = 3
+}
+
+
+resource "aws_instance" "exampleCluster" {
+  ami                    = "ami-045269a1f5c90a6a0"
+  instance_type          = "t2.small"
+  count                  = var.nBroker
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  key_name               = "vockey"
+  user_data = base64encode(templatefile("creation.sh", {
+    idBroker     = "${count.index}"
+    totalBrokers = var.nBroker
+  }))
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "terraform-example-kafka.${count.index}"
+  }
+}
+output "publicdnslist" {
+  value = formatlist("%v", aws_instance.exampleCluster.*.public_dns)
+}
 resource "aws_security_group" "instance" {
   name = var.security_group_name
   ingress {
@@ -37,13 +47,25 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+  ingress {
     from_port   = 2181
     to_port     = 2181
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+  ingress {
+    from_port   = 2888
+    to_port     = 2888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3888
+    to_port     = 3888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     from_port   = 9092
     to_port     = 9092
     protocol    = "tcp"
@@ -57,10 +79,10 @@ resource "aws_security_group" "instance" {
     ipv6_cidr_blocks = ["::/0"]
   }
 }
-
 variable "security_group_name" {
   description = "The name of the security group"
   type        = string
-  default     = "terraform-example-instance91"
+  default     = "terraform-example-instances"
 }
+
 
--- Kafka/creation.sh
+++ Kafka/creation.sh
@@ -8,9 +8,14 @@ sudo mv apache-zookeeper-3.9.3-bin /usr/local/zookeeper
 sudo mkdir -p /var/lib/zookeeper
 echo "tickTime=2000
 dataDir=/var/lib/zookeeper
-clientPort=2181" > /usr/local/zookeeper/conf/zoo.cfg 
+clientPort=2181
+maxClientCnxns=60
+initLimit=10
+syncLimit=5" > /usr/local/zookeeper/conf/zoo.cfg 
 
-sudo yum -y install java-17-amazon-corretto-devel.x86_64  
+sudo yum -y install java-17-amazon-corretto-devel.x86_64
+
+echo ${idBroker} > /var/lib/zookeeper/myid
 
 sudo /usr/local/zookeeper/bin/zkServer.sh start
 
@@ -21,6 +26,14 @@ sudo mkdir /tmp/kafka-logs
 
 ip=`curl http://169.254.169.254/latest/meta-data/public-hostname`
 sudo sed -i "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/$ip:9092/g" /usr/local/kafka/config/server.properties
+
+sudo sed -i "s/broker.id=0/broker.id=${idBroker}/g" /usr/local/kafka/config/server.properties
+
+sudo sed -i "s/offsets.topic.replication.factor=1/offsets.topic.replication.factor=${totalBrokers}/g" /usr/local/kafka/config/server.properties
+
+sudo sed -i "s/transaction.state.log.replication.factor=1/transaction.state.log.replication.factor=${totalBrokers}/g" /usr/local/kafka/config/server.properties
+
+sudo sed -i "s/transaction.state.log.min.isr=1/transaction.state.log.min.isr=${totalBrokers}/g" /usr/local/kafka/config/server.properties
 
 # due to AWS network stablishment process, check if 60 seconds is enough for your situation
 (sleep 120 && sudo /usr/local/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties )&
